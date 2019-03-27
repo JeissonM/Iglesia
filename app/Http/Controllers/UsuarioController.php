6a2d146273d57dd679857700ba0096d229a7abb8
Jeisson Mandon <jorgejeisson@hotmail.com>
2019-03-26 20:09:16 -0500

    Versión 1.13 26 de Marzo 2019 20:08 Jeisson BETA

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\UsuarioRequest;
+use App\User;
+use App\Grupousuario;
+use App\Pagina;
+use App\Modulo;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Auditoriausuario;
 
-class UsuarioController extends Controller
-{
-    //
+class UsuarioController extends Controller {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        $usuarios = User::all();
+        $usuarios->each(function ($usuario) {
+            $usuario->grupousuarios;
+        });
+        return view('usuarios.usuarios.list')
+                        ->with('location', 'usuarios')
+                        ->with('usuarios', $usuarios);
+    }
+
 }
